ddd4a024f50e5cdb0a3db4af5b5cc70fe0e428f7
karol <karol.kowalski95@gmail.com>
2018-01-31 22:47:00 +0100

    Poprawa ortogonalności

--- src/main/scala/cameras/OrthonormalBasis.scala
+++ src/main/scala/cameras/OrthonormalBasis.scala
@@ -5,9 +5,9 @@ import vecmath.Vector
 class OrthonormalBasis(eye: Vector, lookAt: Vector, up: Vector) {
   var w = eye - lookAt
   w = w.normalize
-  val v = w ** u
-  u = u.normalize
   var u = up ** w
+  u = u.normalize
+  val v = w ** u
 
   def *(v: Vector) = u * v.x + this.v * v.y + w * v.z
 }
