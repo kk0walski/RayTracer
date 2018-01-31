5b3e7251cd8937a72a26a12ca29b3bb453dbf6ae
karol <karol.kowalski95@gmail.com>
2018-01-31 22:59:16 +0100

    Refaktoryzacja kodu

--- src/main/scala/shapes/Traingle.scala
+++ src/main/scala/shapes/Traingle.scala
@@ -7,9 +7,7 @@ import vecmath.{Ray, Vector}
 class Traingle(x: Vector, y: Vector, z: Vector, override val color: Color) extends Shape {
 
   override def HitTest(ray: Ray, minDistance: Double): (Boolean, Double) = {
-    val u = y - x;
-    val v = z - x;
-    val n = u ** v
+    val n = (y - x) ** (z - x)
     val t = ((x - ray.origin) * n) / (ray.direction * n)
     if (t > Ray.Epsilon) {
       val p = ray.origin + ray.direction * t
@@ -19,6 +17,7 @@ class Traingle(x: Vector, y: Vector, z: Vector, override val color: Color) exten
   }
 
   def isInTrainle(p: Vector, x: Vector, y: Vector, z: Vector): Boolean = {
+    //Sprawdzenie czy suma pól trójkątów xpz xpy oraz pyz jest równa polu trójkąta xyz
     var reasult = (((p - x) ** (z - x)).length + ((p - x) ** (y - x)).length + ((p - y) ** (z - y)).length) - ((z - x) ** (y - x)).length
     return reasult <= Ray.Epsilon
   }
